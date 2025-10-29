17c9f76a759fc695302a0d5e6b6493430b2024eb
KHANAK M <khanakm369@gmail.com>
2025-10-29 11:26:20 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -8,9 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.use('/api/auth', (await import('./routes/auth.route.js')).default);
 app.use('/api/messages', (await import('./routes/message.route.js')).default);
 
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 let PORT = process.env.PORT || 8080;
 app.listen(3000, () => {
